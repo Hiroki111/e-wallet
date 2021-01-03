888d5d96bb2b134e3386edaf91bfc0d59c73938c
Hiroki Takahashi <git@hiroki-t.com>
2021-01-03 17:28:52 +0100

    initial commit  - built the base of this project

---
+++ server.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.get('/api/customers', cors(), (req, res) => {
+  const customers = [{id: 1, name: 'John'}];
+
+  res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
