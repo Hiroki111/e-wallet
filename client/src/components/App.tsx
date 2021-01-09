9b0b753cb3bac81b11e96b94c7927161488af457
Hiroki Takahashi <git@hiroki-t.com>
2021-01-09 15:41:00 +0100

    Created the base of the login form

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
+import { Home } from 'components/Home';
 import { LoginForm } from 'components/LoginForm';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <LoginForm />
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={LoginForm} />
+      </Router>
     </div>
   );
 };
--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,44 +1,65 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './styles/LoginForm.scss';
 
 export const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  let history = useHistory();
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('submitted', email, password);
-  };
-
-  const renderField = (type: 'email' | 'password') => {
-    let value = undefined;
-    let handler = undefined;
-
-    if (type === 'email') {
-      value = email;
-      handler = handleEmailChange;
-    } else if (type === 'password') {
-      value = password;
-      handler = handlePasswordChange;
-    }
-
-    return (
-      <div className="field">
-        <label htmlFor={type}>{type}</label>
-        <input type={type} value={value} onChange={handler} id={type} name={type} required />
-      </div>
-    );
+    fetch('/api/authenticate', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          history.push('/');
+        } else {
+          const error = new Error('login failed');
+          throw error;
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('Error logging in please try again');
+      });
   };
 
   return (
     <div className="login-form">
       <form onSubmit={handleSubmit}>
         <h3>LOGIN</h3>
-        {renderField('email')}
-        {renderField('password')}
+        <div className="field">
+          <label htmlFor="email">email</label>
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={handleEmailChange}
+            id="email"
+            name="email"
+            required
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={handlePasswordChange}
+            id="password"
+            name="password"
+            required
+          />
+        </div>
         <button className="submit-btn" type="submit">
           Submit
         </button>
