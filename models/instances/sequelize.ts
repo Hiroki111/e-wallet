1c2df73ba263e269ea80c56963749ebdb2518623
Hiroki Takahashi <git@hiroki-t.com>
2021-02-05 11:58:12 +0100

    Removed unnecessary lines

--- models/instances/sequelize.ts
+++ models/instances/sequelize.ts
@@ -1,9 +1,6 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
 
 import config from 'config/config.js';
-
-dotenv.config();
 
 const env = process.env.NODE_ENV || 'development';
 const sequelizeInstance = new Sequelize(config[env].database, config[env].username, config[env].password, config[env]);
--- server.ts
+++ server.ts
@@ -6,12 +6,11 @@ import cors from 'cors';
 
 import { RootRouter } from 'routes';
 
+dotenv.config();
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-dotenv.config();
-
 app.use(cors());
 
 app.use((req, res, next) => {
